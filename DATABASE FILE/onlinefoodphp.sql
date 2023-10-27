b425f5d169b1e392448f298483784b8f5c76f8d1
Vaishnavi <vaishnavigv.sn@gmail.com>
2023-10-27 09:43:49 +0530

    initial database commit4

---
+++ DATABASE FILE/onlinefoodphp.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2022 at 03:22 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinefoodphp`
+--
+
+-- --------------------------------------------------------
+
+
+CREATE TABLE `admin` (
+  `adm_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `code` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
+(1, 'admin', 'CAC29D7A34687EB14B37068EE4708E7B', 'admin@mail.com', '', '2022-05-27 13:21:52');
+
+
+CREATE TABLE `dishes` (
+  `d_id` int(222) NOT NULL,
+  `rs_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `slogan` varchar(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(222),
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
+(1, 1, 'Hyderabad Lamb Patties', 'Lamb patties which melt in your mouth, and are quick and easy to make. Served hot with a crisp salad.', '400.00', '62908867a48e4.jpg'),
+(2, 1, 'Lobster Thermidor', 'Lobster Thermidor is a French dish of lobster meat cooked in a rich wine sauce, stuffed back into a lobster shell, and browned.', '350.00', '629089fee52b9.jpg'),
+(3, 4, 'Chicken Madeira', 'Chicken Madeira is made with chicken, mushrooms, and a special fortified wine.', '500.00', '62908bdf2f581.jpg'),
+(4, 1, 'Stuffed Jacket Potatoes', 'Deep fry whole potatoes in oil for 8-10 minutes or coat each potato with little oil. Mix the onions, garlic, tomatoes and mushrooms. Add yoghurt, ginger, garlic, chillies, coriander', '400.00', '62908d393465b.jpg'),
+(5, 2, 'Pink Spaghetti', 'Spaghetti with prawns in a fresh tomato sauce. This dish originates from Southern Italy and with the combination of prawns, garlic, chilli and pasta. Garnish each with remaining tablespoon parsley.', '450.00', '606d7491a9d13.jpg'),
+(6, 2, 'Cheesy Mashed Potato', 'Deliciously Cheesy Mashed Potato. The ultimate mash for your Thanksgiving table or the perfect accompaniment to vegan sausage casserole. Everyone will love it"s fluffy, cheesy.', '370.00', '606d74c416da5.jpg'),
+(7, 2, 'Crispy Chicken Strips', 'Fried chicken strips, served with special honey mustard sauce.', '340.00', '606d74f6ecbbb.jpg'),
+(8, 2, 'Lemon Grilled Chicken And Pasta', 'Marinated rosemary grilled chicken breast served with mashed potatoes and your choice of pasta.', '500.00', '606d752a209c3.jpg'),
+(9, 3, 'Veg Fried Rice', 'Chinese rice wok with cabbage, beans, carrots, and spring onions.', '250.00', '606d7575798fb.jpg'),
+(10, 3, 'Prawn Crackers', '12 pieces deep-fried prawn crackers', '350.00', '606d75a7e21ec.jpg'),
+(11, 3, 'Spring Rolls', 'Lightly seasoned shredded cabbage, onion and carrots, wrapped in house made spring roll wrappers, deep fried to golden brown.', '200.00', '606d75ce105d0.jpg'),
+(12, 3, 'Manchurian Chicken', 'Chicken pieces slow cooked with spring onions in our house made manchurian style sauce.', '360.00', '606d7600dc54c.jpg'),
+(13, 4, 'Buffalo Wings', 'Fried chicken wings tossed in spicy Buffalo sauce served with crisp celery sticks and Blue cheese dip.', '299.00', '606d765f69a19.jpg'),
+(14, 4, 'Mac N Cheese Bites', 'Served with our traditional spicy queso and marinara sauce.', '320.00', '606d768a1b2a1.jpg'),
+(15, 4, 'Potato Twisters', 'Spiral sliced potatoes, topped with our traditional spicy queso, Monterey Jack cheese, pico de gallo, sour cream and fresh cilantro.', '300.00', '606d76ad0c0cb.jpg'),
+(16, 4, 'Penne Pasta', 'Garlic-herb beef meatballs tossed in our house-made marinara sauce and penne pasta topped with fresh parsley.', '500.00', '606d76eedbb99.jpg');
+
+
+CREATE TABLE `remark` (
+  `id` int(11) NOT NULL,
+  `frm_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `remark` mediumtext NOT NULL,
+  `remarkDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
+(1, 2, 'in process', 'none', '2023-05-01 05:17:49'),
+(2, 3, 'in process', 'none', '2023-05-27 11:01:30'),
+(3, 2, 'closed', 'thank you for your order!', '2023-05-27 11:11:41'),
+(4, 3, 'closed', 'none', '2023-05-27 11:42:35'),
+(5, 4, 'in process', 'none', '2023-05-27 11:42:55'),
+(6, 1, 'rejected', 'none', '2023-05-27 11:43:26'),
+(7, 7, 'in process', 'none', '2023-05-27 13:03:24'),
+(8, 8, 'in process', 'none', '2023-05-27 13:03:38'),
+(9, 9, 'rejected', 'thank you', '2023-05-27 13:03:53'),
+(10, 7, 'closed', 'thank you for your ordering with us', '2023-05-27 13:04:33'),
+(11, 8, 'closed', 'thanks ', '2023-05-27 13:05:24'),
+(12, 5, 'closed', 'none', '2023-05-27 13:18:03');
+
+
+CREATE TABLE `restaurant` (
+  `rs_id` int(222) NOT NULL,
+  `c_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `url` varchar(222) NOT NULL,
+  `o_hr` varchar(222) NOT NULL,
+  `c_hr` varchar(222) NOT NULL,
+  `o_days` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `image` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
+(1, 1, 'North Street Tavern', 'nthavern@mail.com', '3547854700', 'www.northstreettavern.com', '8am', '9pm', 'mon-sat', 'Jayanagar, Bangalore', '6290877b473ce.jpg', '2023-05-27 08:10:35'),
+(2, 2, 'Eataly', 'eataly@gmail.com', '0557426406', 'www.eataly.com', '11am', '9pm', 'Mon-Sat', 'Banashankari II stage, Bangalore', '606d720b5fc71.jpg', '2023-05-27 08:06:41'),
+(3, 3, 'Chinese Mainland', 'chnmnld@mail.com', '1458745855', 'www.nanxiangbao45.com', '9am', '10pm', 'mon-sat', 'JP Nagar, Bangalore', '6290860e72d1e.jpg', '2023-05-27 08:04:30'),
+(4, 4, 'Highlands Bar & Grill', 'hbg@mail.com', '6545687458', 'www.hbg.com', '7am', '9pm', 'mon-sat', 'MG Road, Bangalore', '6290af6f81887.jpg', '2023-05-27 11:01:03');
+
+
+CREATE TABLE `res_category` (
+  `c_id` int(222) NOT NULL,
+  `c_name` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/* insert into `res_category` */
+
+
+CREATE TABLE `users` (
+  `u_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `f_name` varchar(222) NOT NULL,
+  `l_name` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `status` int(222) NOT NULL DEFAULT '1',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+/* insert into `users` */
+
+
+CREATE TABLE `users_orders` (
+  `o_id` int(222) NOT NULL,
+  `u_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `quantity` int(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` varchar(222) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/* insert into `users_orders` */
+
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+ALTER TABLE `dishes`
+  ADD PRIMARY KEY (`d_id`);
+
+
+ALTER TABLE `remark`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`rs_id`);
+
+
+ALTER TABLE `res_category`
+  ADD PRIMARY KEY (`c_id`);
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`o_id`);
+
+
+
+
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `dishes`
+  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+ALTER TABLE `remark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+ALTER TABLE `restaurant`
+  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+ALTER TABLE `res_category`
+  MODIFY `c_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+ALTER TABLE `users`
+  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+ALTER TABLE `users_orders`
+  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
